6c6147c3c01986f98d324862fa362917ab9e11d5
Ola van Dunen <vandunen@envisioning.solutions>
2025-10-22 11:50:11 +0200

    removes grade package

--- src/main/java/ao/co/oportunidade/Amount.java
+++ src/main/java/ao/co/oportunidade/Amount.java
@@ -3,10 +3,12 @@ package ao.co.oportunidade;
 import ao.co.oportunidade.valueobject.ValueObject;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 
 @RequiredArgsConstructor
 @Getter
+@Setter
 public class Amount implements ValueObject  {
 
     private final Double amount;
